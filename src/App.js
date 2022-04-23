59d8cf15f049ff0f5f50e99d2512b1eb5c62c14e
Valerii Bihun <valeriibihun.co@gmail.com>
2022-04-23 00:00:16 -0600

    Adding components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import "./App.css";
 import Machine from "./Machine/Machine";
+import Chips from "./Product/Chips";
+import Sardines from "./Product/Sardines";
+import Soda from "./Product/Soda";
+
 
 function App() {
   return <div className="App">
