701b8139f04c298f3103896f49400effc78ff3ee
Valerii Bihun <valeriibihun.co@gmail.com>
2022-04-24 17:34:01 -0600

    added go back button to component foods

--- src/Product/Chips.jsx
+++ src/Product/Chips.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Message from '../Message/Message';
+import { Link } from "react-router-dom";
+
+import "./Chips.css";
+
 
 const Chips = () => {
   return (
-    <div>Chips</div>
+    <div className='Chips'>
+        <Message>
+          <h1>
+            <Link to='/'>Go Back</Link>
+          </h1>
+        </Message>
+        
+      </div>
   )
 }
 
