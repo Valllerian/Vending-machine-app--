ea1ba102d373804d01193f1b31c24de2c52cc6a3
Valerii Bihun <valeriibihun.co@gmail.com>
2022-04-24 17:25:43 -0600

    Message component added to the main page

--- src/Machine/Machine.jsx
+++ src/Machine/Machine.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import Message from '../Message/Message';
+import vendingMachineImg from "./VendingMachine.png";
+import "./VendingMachine.css";
 
 const Machine = () => {
   return (
-    <div>Machine</div>
+    <div
+        className='VendingMachine'
+        style={{ backgroundImage: `url(${vendingMachineImg})` }}
+      >
+        <Message>
+          <h1>hello i am a vending machine. what would you like to eat?</h1>
+        </Message>
+        <Message>
+          <h1>
+            <Link to='/soda'>Soda</Link>
+          </h1>
+          <h1>
+            <Link to='/chips'>Chips</Link>
+          </h1>
+          <h1>
+            <Link to='/sardines'>Sardines</Link>
+          </h1>
+        </Message>
+      </div>
   )
 }
 
---
+++ src/Message/Message.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Message = (props) => {
+  return (
+    <div className='Message'>{props.children}</div>
+  )
+}
+
+export default Message
