f6a586f9d25ddbe4b6a91c477bde12795bd60051
Valerii Bihun <valeriibihun.co@gmail.com>
2022-04-24 17:18:40 -0600

    styled navbar added

--- src/App.js
+++ src/App.js
@@ -4,22 +4,22 @@ import Chips from "./Product/Chips";
 import Sardines from "./Product/Sardines";
 import Soda from "./Product/Soda";
 import Navbar from "./Navbar/Navbar";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <div className="App">
-     
-     <Router>
-     <Navbar />
-     <Routes>
-          <Route exact path='/'element={<Machine/>} />
-          <Route exact path='/soda' element={<Soda/>} />
-          <Route exact path='/sardines' element={<Sardines/>} />
-          <Route exact path='/chips' element={<Chips/>} />
-          </Routes>
-          </Router>
-  </div>;
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Machine />} />
+          <Route exact path="/soda" element={<Soda />} />
+          <Route exact path="/sardines" element={<Sardines />} />
+          <Route exact path="/chips" element={<Chips />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
