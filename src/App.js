78acd35683459275b594b1b32be5cc9a9ccd0869
Valerii Bihun <valeriibihun.co@gmail.com>
2022-04-23 23:29:40 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import Machine from "./Machine/Machine";
 import Chips from "./Product/Chips";
 import Sardines from "./Product/Sardines";
 import Soda from "./Product/Soda";
-
+import Navbar from "./Navbar/Navbar";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return <div className="App">
