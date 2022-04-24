c7511582f61a5bdb34891512c3591b73daa237c7
Valerii Bihun <valeriibihun.co@gmail.com>
2022-04-24 17:07:45 -0600

    added working navbar

--- src/App.js
+++ src/App.js
@@ -8,7 +8,13 @@ import { Route, Switch } from "react-router-dom";
 
 function App() {
   return <div className="App">
-    <Machine />
+     <Navbar />
+        <Switch>
+          <Route exact path='/' render={() => <VendingMachine />} />
+          <Route exact path='/soda' render={() => <Soda />} />
+          <Route exact path='/sardines' render={() => <Sardines />} />
+          <Route exact path='/chips' render={() => <Chips />} />
+        </Switch>
   </div>;
 }
 
