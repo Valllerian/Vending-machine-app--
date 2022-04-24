150a524f1cb01de09cd8c5958784f551422c8eb8
Valerii Bihun <valeriibihun.co@gmail.com>
2022-04-24 17:16:26 -0600

    refactored Routes to the current version

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import Chips from "./Product/Chips";
 import Sardines from "./Product/Sardines";
 import Soda from "./Product/Soda";
 import Navbar from "./Navbar/Navbar";
-import { Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
 
 function App() {
   return <div className="App">
+     
+     <Router>
      <Navbar />
-        <Switch>
-          <Route exact path='/' render={() => <VendingMachine />} />
-          <Route exact path='/soda' render={() => <Soda />} />
-          <Route exact path='/sardines' render={() => <Sardines />} />
-          <Route exact path='/chips' render={() => <Chips />} />
-        </Switch>
+     <Routes>
+          <Route exact path='/'element={<Machine/>} />
+          <Route exact path='/soda' element={<Soda/>} />
+          <Route exact path='/sardines' element={<Sardines/>} />
+          <Route exact path='/chips' element={<Chips/>} />
+          </Routes>
+          </Router>
   </div>;
 }
 
